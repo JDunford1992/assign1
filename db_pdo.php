0e67e905a417b66de2f1b08847da8c9f2d4026b9
JDunford1992 <s3330661@student.rmit.edu.au>
2013-09-01 15:21:17 +1000

    Create db_pdo.php

---
+++ db_pdo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * MySQL connection check
+ *
+ */
+
+define('DB_NAME',   'winestore');
+define('DB_USER',   'webadmin');
+define('DB_PW',     s3330661);
+$databaseName=DB_NAME;
+$hostName='localhost';
+$dsn = "mysql:dbname=$databaseName;host=$hostName;port=52247";
+?>
